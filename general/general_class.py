420c260c1cd873671554940ddf79763028369f76
Favalon <bleedavalon@gmail.com>
2020-01-29 01:53:36 +0000

    regular update

--- general/general_class.py
+++ general/general_class.py
@@ -13,6 +13,7 @@ class Movie:
 
         self.scene_len = None
         self.status = None
+        self.main_status = None
         self.path = None
         self.url = None
         self.initial_url()
@@ -24,7 +25,8 @@ class Movie:
     def initial_status(self, status):
         self.scene_len = status.shape[0]
         self.status = status
-        pass
+
+        self.main_status = status[:, self.main_char['index'], :]
 
     def initial_path(self):
         status = self.status
@@ -42,10 +44,12 @@ class Movie:
 
 
 class MoviesData:
-    def __init__(self, status, path):
-        self.status = status
-        self.path = path
+    def __init__(self, movies):
+        self.movies = movies
 
-    def initial_any(self):
+    def status_path_all_freq(self):
+        pass
+
+    def separate_main_char(self):
         pass
 
